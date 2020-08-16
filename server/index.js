690c00db38eb662d9eeaef520b2f949705f634e3
Priam Jain <priamjain8@gmail.com>
2020-08-16 18:00:59 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ const socketio = require('socket.io');
 const http = require('http');
 const { addUser, removeUser, getUser, getUsersInParty } = require('./Users');
 const app = express();
+const cors = require('cors');
 
 const server = http.createServer(app);
 const io = socketio(server);
 const port  = process.env.PORT || 5000;
 const router  = require('./router');
 app.use(router);
-
+app.use(cors());
 io.on('connect', (socket) => {
   socket.on('join', ({ name, party }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, party });
@@ -20,7 +21,7 @@ io.on('connect', (socket) => {
 
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to party ${user.party}.`});
     socket.broadcast.to(user.party).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
+    io.to(user.party).emit('partyData', { party: user.party, users: getUsersInParty(user.party)});
     callback();
   });
 
