3db0780ba068df918ae746b2008e79fd875cf455
Priam Jain <priamjain8@gmail.com>
2020-08-30 13:39:47 +0530

    no cors

--- server/index.js
+++ server/index.js
@@ -20,8 +20,7 @@ const io = socketio(server, {
 const port  = process.env.PORT || 5000;
 const router  = require('./router');
 app.use(router);
-app.use(cors());
-io.set('origins', "https://hangout.netlify.app");
+// app.use(cors());
 io.on('connect', (socket) => {
   socket.on('join', ({ name, party }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, party });
